80b369d6dff46af773a3e9194df10d219843db11
juneunbae <dmsqo1403@naver.com>
2026-01-25 10:31:58 +0900

    19. Link 컴포넌트 설명

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,12 @@
 import "@/styles/globals.css";
-import Link from "next/dist/client/link";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }) {
   return (
     <div>
       <div>
         <nav>
-          <Link href={"/home"}>Home</Link>|<Link href={"/login"}>Login</Link>
+          <Link href={"/home"}>Home</Link> | <Link href={"/login"}>Login</Link>
         </nav>
       </div>
       <Component {...pageProps} />
