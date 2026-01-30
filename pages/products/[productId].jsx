dbd4a1b86f1fdd4eee2baac345a771a422831d9c
juneunbae <dmsqo1403@naver.com>
2026-01-30 21:39:01 +0900

    34. 동적 라우팅 id 값 확인

--- pages/products/[productId].jsx
+++ pages/products/[productId].jsx
@@ -3,3 +3,8 @@ import React from "react";
 export default function ProductDetailPage() {
   return <div>ProductDetailPage</div>;
 }
+
+export async function getServerSideProps(context) {
+  // /products/15
+  console.log(context.params.productId);
+}
