45fe1dbdf98e8fe6a1c7034b452734931e00ebd6
juneunbae <dmsqo1403@naver.com>
2026-01-31 13:02:06 +0900

    40. getServerSideProps로 데이터 호출 및 화면에 표시

--- pages/products/[productId].jsx
+++ pages/products/[productId].jsx
@@ -1,14 +1,16 @@
-import React from "react";
 import ProductHeader from "@/components/ProductHeader";
+import React from "react";
+import axios from "axios";
 
 // 상품 상세 정보 페이지 컴포넌트
-export default function ProductDetailPage({ msg }) {
+export default function ProductDetailPage({ msg, productInfo }) {
   const headerTitle = "상품 상세 정보 페이지";
 
   return (
     <div>
       <ProductHeader title={headerTitle}></ProductHeader>
       <div>ProductDetailPage - {msg}</div>
+      <p>{productInfo.name}</p>
     </div>
   );
 }
@@ -16,8 +18,13 @@ export default function ProductDetailPage({ msg }) {
 export async function getServerSideProps(context) {
   // /products/15
   console.log(context.params.productId);
+  const id = context.params.productId;
+  const response = await axios.get(`http://localhost:4000/products/${id}`);
 
   return {
-    props: { msg: "서버  데이터" },
+    props: {
+      msg: "서버  데이터",
+      productInfo: response.data,
+    },
   };
 }
