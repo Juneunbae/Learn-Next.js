285e0690dd5e23fde8a75d6b43e74ce7251e0304
juneunbae <dmsqo1403@naver.com>
2026-01-31 12:53:42 +0900

    39. props와 getServerSideProps 차이

--- pages/products/[productId].jsx
+++ pages/products/[productId].jsx
@@ -4,6 +4,7 @@ import ProductHeader from "@/components/ProductHeader";
 // 상품 상세 정보 페이지 컴포넌트
 export default function ProductDetailPage({ msg }) {
   const headerTitle = "상품 상세 정보 페이지";
+
   return (
     <div>
       <ProductHeader title={headerTitle}></ProductHeader>
