a67f4e89465c58f5f094b654c74c066946ff473f
juneunbae <dmsqo1403@naver.com>
2026-02-07 23:49:34 +0900

    50. 이미지 성능 최적화

--- components/ProductInfo.jsx
+++ components/ProductInfo.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from "./ProductInfo.module.css";
+import Image from "next/image";
 
 export default function ProductInfo({ productDetail }) {
   return (
     <div className={styles.container}>
       <div>
-        <img src={productDetail.imageUrl} alt="" />
+        <Image
+          src={productDetail.imageUrl}
+          alt="{productDetail.name}"
+          width={250}
+          height={250}
+        />
       </div>
       <div className={styles.description}>
         <p>{productDetail.name}</p>
