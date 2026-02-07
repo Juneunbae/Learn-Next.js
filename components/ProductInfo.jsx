774cc3f13a9a8a96d3fb8995080ec12cdad5c696
juneunbae <dmsqo1403@naver.com>
2026-02-07 23:43:34 +0900

    49. 상품 상세 페이지 UI 스타일링

--- components/ProductInfo.jsx
+++ components/ProductInfo.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import styles from "./ProductInfo.module.css";
 
 export default function ProductInfo({ productDetail }) {
   return (
-    <div>
+    <div className={styles.container}>
       <div>
         <img src={productDetail.imageUrl} alt="" />
       </div>
-      <p>{productDetail.name}</p>
-      <p>{productDetail.price}</p>
+      <div className={styles.description}>
+        <p>{productDetail.name}</p>
+        <p>{productDetail.price}</p>
+        <button>장바구니에 담기</button>
+      </div>
     </div>
   );
 }
