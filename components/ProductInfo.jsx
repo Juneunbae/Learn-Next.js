b230521d9fa6fdfa9a99dc74b2383c984fe80784
juneunbae <dmsqo1403@naver.com>
2026-02-08 16:15:07 +0900

    52. 장바구니 아이템 추가 API 구현

--- components/ProductInfo.jsx
+++ components/ProductInfo.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import styles from "./ProductInfo.module.css";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { createCartItem } from "@/api";
 
 export default function ProductInfo({ productDetail }) {
   const router = useRouter();
 
-  const addCart = () => {
+  const addCart = async () => {
     // 1. 장바구니에 아이템을 담는 API 함수 호출
     // 2. 장바구니 페이지로 이동
-    router.push("/cart");
+    const response = await createCartItem(productDetail.id, productDetail.name);
+    console.log(response);
+    alert("장바구니에 추가됨");
+    await router.push("/cart");
   };
+
   return (
     <div className={styles.container}>
       <div>
