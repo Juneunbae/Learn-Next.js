d9a346177b98524e38573b0d5efa9c5da79e84f6
juneunbae <dmsqo1403@naver.com>
2026-02-10 21:21:30 +0900

    59. 장바구니 목록 UI 스타일링

--- components/cart/CartList.jsx
+++ components/cart/CartList.jsx
@@ -1,10 +1,29 @@
+import Image from "next/image";
+import React from "react";
+import styles from "./CartList.module.css";
+
 export default function CartList({ carts }) {
   return (
     <div>
       <div>
         <ul>
           {carts.map((cart) => {
-            return <li key={cart.id}>{cart.name}</li>;
+            return (
+              <li key={cart.id} className={styles.item}>
+                <div>
+                  <Image
+                    src={cart.imageUrl}
+                    alt={cart.name}
+                    width={75}
+                    height={75}
+                  ></Image>
+                </div>
+                <div className={styles.description}>
+                  <div>{cart.name}</div>
+                  <div>{cart.price}</div>
+                </div>
+              </li>
+            );
           })}
         </ul>
       </div>
