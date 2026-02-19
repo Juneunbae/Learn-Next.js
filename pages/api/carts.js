ea76f0a0dbb28f95f946903bcb332c9bd4e86931
juneunbae <dmsqo1403@naver.com>
2026-02-19 21:25:31 +0900

    66. API 라우트를 이용한 장바구니 삭제 기능 구현

---
+++ pages/api/carts.js
@@ -0,0 +1,8 @@
+// http://localhost:3000/api/carts
+import { removeCartItem } from "@/api";
+
+export default async function handler(req, res) {
+  const id = req.body.id;
+  const { data } = await removeCartItem(id);
+  res.status(200).send(`${data.name} 삭제가 되었습니다.`);
+}
